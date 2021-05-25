915621f18c48e3e822b1e84aead7f1d8e498daab
John Yevsukov <github.com@john.engineering>
2021-05-24 22:12:25 -0400

    built out create class comp and home comp

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,7 +3,10 @@ import React, { useState, useEffect } from "react";
 import { Route, Link, Switch } from "react-router-dom";
 //STLYING IMPORTS
 import "../src/index.css";
-import logo from "../src/weight.PNG" 
+import logo from "../src/weight.PNG"
+//COMPONENT IMPORTS
+import CreateClass from './components/Classes/CreateClass'
+import Home from './components/Home/Home'
 
 function App() {
   return (
@@ -11,6 +14,25 @@ function App() {
       <div className="mainHeadingLogoCombo">
       <h1>Anywhere Fitness</h1>
       <img className="logo" src={logo} alt="gym kettlebell" />
+      <Switch>
+
+        <Route exact path='/'>
+          <Home/>
+        </Route>
+
+        {/* <Route path = "/login">
+          <Login/>
+        </Route>
+
+        <Route path = "/signup">
+          <Signup/>
+        </Route> */}
+
+        <Route path='/create-class'>
+          <CreateClass/>
+        </Route>
+
+      </Switch>
       </div>
     </div>
   );
