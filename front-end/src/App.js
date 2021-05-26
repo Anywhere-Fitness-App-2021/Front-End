53145dce9021b112304643b03b4c95f723d10b17
cynthiaannberg <cynthia.ann.berg@me.com>
2021-05-26 13:20:13 -0600

    john merge

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -39,3 +39,42 @@ function App() {
 }
 
 export default App;
+
+// import "../src/index.css";
+// import logo from "../src/weight.PNG"
+// //COMPONENT IMPORTS
+// import CreateClass from './components/Classes/CreateClass'
+// import Home from './components/Home/Home'
+// import CreateClass from './components/CreateClass'
+// import Home from './components/Home'
+// import EditClass from './components/EditClass'
+// import InstructorPage from './components/InstructorPage'
+
+// function App() {
+//   return (
+// @@ -28,8 +30,21 @@ function App() {
+//           <Signup/>
+//         </Route> */}
+
+
+//         {/* <Route exact path = "/instructor">
+//           <InstructorPage/>
+//         </Route>
+//         <Route path='/create-class'>
+//           <CreateClass/>
+//         </Route> */}
+
+//         <Route path = "/instructor">
+//           <InstructorPage/>
+//         </Route>
+
+//         <Route path = "/instructor/edit-class/:ClassId">
+//           <EditClass/>
+//         </Route>
+
+//       </Switch>
+//       </div>
+//     </div>
+//   );
+// }
+// export default App;
